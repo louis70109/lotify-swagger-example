04b70a5944f81ec9ecdd378e2434472dc14701ea
NiJia <louis70109@gmail.com>
2020-09-27 21:25:11 +0800

    chore: set env config

--- api.py
+++ api.py
@@ -4,28 +4,32 @@ from flask import Flask
 from flask_cors import CORS
 from flask_restful_swagger_2 import Api
 
-from controller.authorizer_controller import RootController, CallbackController, RevokeTokenController
+from controller.authorizer_controller import RootController, CallbackController, RevokeTokenController, LinkController, \
+    CodeChangeController
 from controller.event_controller import TextController, StickerController, ImageUrlController, ImageFileController
 
 app = Flask(__name__)
 CORS(app, resources={r"*": {"origins": "*", "supports_credentials": True}})
 
-local = os.environ.get('IS_OFFLINE')
+DOMAIN = os.environ.get('DOMAIN')
+PORT = os.environ.get('PORT') or 5000
 
 
 def is_local():
-    return True if local else False
+    return True if DOMAIN else False
 
 
-# https://github.com/swagger-api/swagger-codegen/issues/7847#issuecomment-374512375
 api = Api(app,
-          host='localhost:5000' if local else '0.0.0.0:5000',
-          schemes=['http' if local else 'https'],
+          host=f'localhost:{PORT}' if is_local else DOMAIN,
+          schemes=['http' if is_local else 'https'],
           base_path='/',
           api_version='0.0.1',
           api_spec_url='/api/swagger')
+
 api.add_resource(RootController, "/")
 api.add_resource(CallbackController, "/callback")
+api.add_resource(LinkController, "/notify/link")
+api.add_resource(CodeChangeController, "/notify/change")
 api.add_resource(RevokeTokenController, "/notify/revoke")
 api.add_resource(TextController, "/notify/send")
 api.add_resource(StickerController, "/notify/sticker")
@@ -33,4 +37,4 @@ api.add_resource(ImageUrlController, "/notify/url")
 api.add_resource(ImageFileController, "/notify/file")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=PORT, debug=True)
