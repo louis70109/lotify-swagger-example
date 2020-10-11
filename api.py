98dc4f0b976f3ea41e088a29db0ddda5e2d48d91
NiJia <louis70109@gmail.com>
2020-10-11 19:12:45 +0800

    fix function error

--- api.py
+++ api.py
@@ -20,8 +20,8 @@ def is_local():
 
 
 api = Api(app,
-          host=f'localhost:{PORT}' if is_local else DOMAIN,
-          schemes=['http' if is_local else 'https'],
+          host=f'localhost:{PORT}' if is_local() else DOMAIN,
+          schemes=['http' if is_local() else 'https'],
           base_path='/',
           api_version='0.0.1',
           api_spec_url='/api/swagger')
